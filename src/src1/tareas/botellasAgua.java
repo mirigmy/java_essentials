7545d9a1dfb58a3a051566539ee6455be4817747
Mirigmy <mirigmy@hotmail.com>
2020-03-02 19:17:35 -0600

    se arreglo el bug de los numeros negativos del programa de botellas de agua

--- src/src1/tareas/botellasAgua.java
+++ src/src1/tareas/botellasAgua.java
@@ -5,9 +5,9 @@ import libs.Input;
 public class botellasAgua {
 
 	public static void main(String[] args) {
-		System.out.print("Hola: Me puedes decir cuánto tiempo tardas en bañarte? ");
+		System.out.println("Hola: \nMe puedes decir cuánto tiempo tardas en bañarte? ");
 		int minutos;
-		minutos = Input.get_int();
+		minutos = Input.get_possitive_int();
 		int botellasAgua;
 		botellasAgua = minutos * 12;
 		System.out.print("Aah tu gastas: " + botellasAgua + " botellas de agua");
