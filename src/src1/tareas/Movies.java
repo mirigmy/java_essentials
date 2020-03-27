8dcfb2e4afb12275f7c5273527a759c136ceb5cc
Mirigmy <mirigmy@hotmail.com>
2020-03-27 01:15:01 -0600

    se creo una clase junto con nueva clase que se llama Movie

---
+++ src/src1/tareas/Movies.java
@@ -0,0 +1,15 @@
+package src1.tareas;
+
+public class Movies {
+	String titulo;
+	String genero;
+	int duracion;
+	
+	public void inicio() {
+		System.out.println ("Inicia la pelicula");
+	}
+	public void pelicula() {
+		System.out.println ("Vas a ver la peli: " + titulo);
+		System.out.println ("");
+	}
+}
