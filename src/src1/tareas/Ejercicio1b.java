cfd4629ec0f9dd540ce82603c69869749f7e84e8
Mirigmy <mirigmy@hotmail.com>
2020-03-26 21:12:38 -0600

    se añadio programa de ejercicios 1b del libro de java

---
+++ src/src1/tareas/Ejercicio1b.java
@@ -0,0 +1,35 @@
+package src1.tareas;
+
+class Ejercicio1b {
+/*	public static void main(String [] args) {
+		int x = 1;  // NOSE VA  IMPRIMIR PORQUE EL VALOR DE X ES 1
+		while ( x < 10 ) { // COMO 1 ES MENOR QUE 10
+			if ( x > 3) { // 1 ES MENOS A 3 NO SE CUMPLE LA CONDICION, ENTONCES SALE
+				System.out.println("big x");
+			}
+		}
+	}
+*/
+	public static void main(String [] args) {
+	/*    int x = 5;
+	    while ( x > 1 ) {
+	      x = x - 1;
+	      if ( x < 3) {
+	        System.out.println("small x"); // Se imprime dos veces porque son las veces en que 1 es menor a 3 y 2 es menor a 3
+	    }
+	  } */
+	
+	//	class Exercise1b { //En el ejercicio falt aponer el void main para que funcione bien
+	    int x = 5;
+	    while ( x > 1 ) {
+	      x = x - 1;
+	      if ( x < 3) {
+	        System.out.println("small x");
+	      }
+	    }
+	}
+}
+	
+
+	
+
